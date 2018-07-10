e49577e7746d1bf3081a659bcbdaa5ca0232ea87
madhuv2002 <madhuv2002@gmail.com>
2018-07-10 15:35:13 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,54 @@
 
 # Code your methods below
+
+def assume_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end 
+
+def bust_a_move
+  puts "be cool"
+  puts "be snazzy"
+end
+
+def base_footwork
+    puts "lift right foot"
+    puts "Return right foot to the ground"
+    puts "Finishing with a small skip-step backward"
+end
+
+def skip_step_left
+    puts "Lower left foot to the ground"
+    puts "Bounce left foot back up slightly, kicking it a few inches back"
+    puts "Left, Right, Left and Left"
+end
+
+def skip_step_right
+    puts "Lower right foot to the ground"
+    puts "Bounce right foot back up slightly, kicking it a few inches back"
+    puts "Right, Left, Right and Right"
+end
+
+
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end 
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end 
+
+assume_stance
+bust_a_move
+base_footwork
+skip_step_left
+skip_step_right
+bob_the_reins
+skip_step_left
+skip_step_right
+lasso
+skip_step_left
+skip_step_right
